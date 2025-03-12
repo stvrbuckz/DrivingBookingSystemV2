fc8f7476732ed157c4518d8f079902da1336858e
Priya Bhatti <69536106+stvrbuckz@users.noreply.github.com>
2025-03-12 15:39:26 +0000

    Add project files.

---
+++ DrivingBookingSystemV2/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DrivingBookingSystemV2.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
