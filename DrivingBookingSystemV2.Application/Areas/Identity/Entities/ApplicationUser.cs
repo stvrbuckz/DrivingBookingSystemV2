5173df0ad6b4735dfee0b4b96fd45a932223af7e
Priya Bhatti <69536106+stvrbuckz@users.noreply.github.com>
2025-04-21 15:34:26 +0100

    Extending IdentityUser class

---
+++ DrivingBookingSystemV2.Application/Areas/Identity/Entities/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DrivingBookingSystemV2.Application.Areas.Identity.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public int EthnicityID { get; set; }
+
+        public int GenderID { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public bool Inactive { get; set; }
+
+    }
+}
